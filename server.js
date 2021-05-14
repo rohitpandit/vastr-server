2d128f78e9eb738784085c7f35fa271bea2a0583
Rohit Pandit <rohit0311@outlook.com>
2021-05-14 20:20:57 +0530

    adds basic server setup with express

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log('Sever live at port: ', PORT);
+});
