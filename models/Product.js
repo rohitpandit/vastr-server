468f691b5cbcc348f28907ee59b77ab3eecf4cb1
Rohit Pandit <rohit0311@outlook.com>
2021-05-16 21:32:03 +0530

    adds productSchema

---
+++ models/Product.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+	category: {
+		type: String,
+		required: true,
+	},
+	url: {
+		type: String,
+		required: true,
+	},
+	desc: {
+		type: String,
+		required: true,
+	},
+	quantity: {
+		type: Number,
+		required: true,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model('product', productSchema);
