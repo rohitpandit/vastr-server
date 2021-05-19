c2683dc848d3c152063e6198e0775cda57aa67b2
Rohit Pandit <rohit0311@outlook.com>
2021-05-19 13:00:51 +0530

    adds post user route

--- routes/user.js
+++ routes/user.js
@@ -17,4 +17,30 @@ router.get('/', async (req, res) => {
 	}
 });
 
+router.post('/', async (req, res) => {
+	try {
+		if (!req.userId) {
+			res.status(401).json({ message: 'Unauthorized user!' });
+			return;
+		}
+
+		const { address, name } = req.body;
+		const user = await User.findOne({ _id: req.userId });
+
+		if (address) {
+			user.address = address;
+		}
+		if (name) {
+			user.name = name;
+		}
+
+		await user.save();
+
+		res.status(200).json({ user: user });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Some internal error occured' });
+	}
+});
+
 module.exports = router;
