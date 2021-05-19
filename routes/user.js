85285b79961cfbd441d8cfdfae4e3c91dcb3faf0
Rohit Pandit <rohit0311@outlook.com>
2021-05-19 12:57:20 +0530

    adds get user route

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-router.get('/', (req, res) => {
-	res.send('hi');
+router.get('/', async (req, res) => {
+	try {
+		if (!req.userId) {
+			res.status(401).json({ message: 'Unauthorized user!' });
+			return;
+		}
+
+		const user = await User.findOne({ _id: req.userId });
+		res.status(200).json({ user: user });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: 'Some internal error occured' });
+	}
 });
 
 module.exports = router;
