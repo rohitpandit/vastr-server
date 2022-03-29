66741454234340257c7a98b0c94b571b9198c6ee
Rohit Pandit <rohit0311@outlook.com>
2022-03-29 14:56:27 +0530

    removes secret from env variable

--- middlewares/verifyUser.js
+++ middlewares/verifyUser.js
@@ -1,18 +1,18 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
 const verifyUser = (req, res, next) => {
-	try {
-		if (req.headers['authorization']) {
-			const token = req.headers['authorization'].split(' ')[1];
-			const verify = jwt.verify(token, process.env.JWTSECRET);
-			if (verify) {
-				req.userId = verify._id;
-			}
-		}
-	} catch (error) {
-		res.status(401).json({ message: 'unauthorized user' });
-	}
-	next();
-};
+  try {
+    if (req.headers['authorization']) {
+      const token = req.headers['authorization'].split(' ')[1]
+      const verify = jwt.verify(token, 'jwtSecret')
+      if (verify) {
+        req.userId = verify._id
+      }
+    }
+  } catch (error) {
+    res.status(401).json({ message: 'unauthorized user' })
+  }
+  next()
+}
 
-module.exports = verifyUser;
+module.exports = verifyUser
--- routes/auth.js
+++ routes/auth.js
@@ -1,68 +1,68 @@
-const express = require('express');
-const User = require('../models/User');
-const router = express.Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const User = require('../models/User')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 router.post('/login', async (req, res) => {
-	try {
-		const { email, password } = req.body;
+  try {
+    const { email, password } = req.body
 
-		if (!email || !password) {
-			res.status(400).json({ message: 'Enter email and password' });
-			return;
-		}
+    if (!email || !password) {
+      res.status(400).json({ message: 'Enter email and password' })
+      return
+    }
 
-		const user = await User.findOne({ email: email });
-		if (!user) {
-			res.status(400).json({ message: 'Wrong email or password' });
-			return;
-		}
+    const user = await User.findOne({ email: email })
+    if (!user) {
+      res.status(400).json({ message: 'Wrong email or password' })
+      return
+    }
 
-		const passwordCheck = await bcrypt.compare(password, user.password);
+    const passwordCheck = await bcrypt.compare(password, user.password)
 
-		if (!passwordCheck) {
-			res.status(400).json({ message: 'Wrong email or password' });
-			return;
-		}
+    if (!passwordCheck) {
+      res.status(400).json({ message: 'Wrong email or password' })
+      return
+    }
 
-		const token = jwt.sign({ _id: user._id }, process.env.JWTSECRET);
+    const token = jwt.sign({ _id: user._id }, 'jwtSecret')
 
-		res.status(200).json({ token: token, admin: user.admin });
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: 'Some internal error occured!' });
-	}
-});
+    res.status(200).json({ token: token, admin: user.admin })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Some internal error occured!' })
+  }
+})
 
 router.post('/signup', async (req, res) => {
-	try {
-		const { email, password } = req.body;
-		console.log(email, password);
+  try {
+    const { email, password } = req.body
+    console.log(email, password)
 
-		if (!email || !password) {
-			res.status(400).json({ message: 'Enter email and password' });
-			return;
-		}
+    if (!email || !password) {
+      res.status(400).json({ message: 'Enter email and password' })
+      return
+    }
 
-		let user = await User.findOne({ email: email });
-		if (user !== null) {
-			res.status(400).json({ message: 'Email not available!' });
-			return;
-		}
+    let user = await User.findOne({ email: email })
+    if (user !== null) {
+      res.status(400).json({ message: 'Email not available!' })
+      return
+    }
 
-		const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10)
 
-		const newUser = await User({ email, password: hashedPassword });
-		await newUser.save();
+    const newUser = await User({ email, password: hashedPassword })
+    await newUser.save()
 
-		const token = jwt.sign({ _id: newUser._id }, process.env.JWTSECRET);
+    const token = jwt.sign({ _id: newUser._id }, 'jwtSecret')
 
-		res.status(200).json({ token: token });
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ message: 'Some internal error occured!' });
-	}
-});
+    res.status(200).json({ token: token })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Some internal error occured!' })
+  }
+})
 
-module.exports = router;
+module.exports = router
