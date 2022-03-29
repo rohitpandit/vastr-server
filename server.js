e22ae5a724bee811605b8404fb489b3a1ad4ba0f
Rohit Pandit <rohit0311@outlook.com>
2022-03-29 14:35:50 +0530

    minor changes

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-require('./db');
-require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+require('./db')
+require('dotenv').config()
 
-const auth = require('./routes/auth');
-const product = require('./routes/product');
-const user = require('./routes/user');
-const order = require('./routes/order');
-const stripe = require('./routes/stripe');
-const verifyUser = require('./middlewares/verifyUser');
+const auth = require('./routes/auth')
+const product = require('./routes/product')
+const user = require('./routes/user')
+const order = require('./routes/order')
+const stripe = require('./routes/stripe')
+const verifyUser = require('./middlewares/verifyUser')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000
 
-app.use('/auth', auth);
-app.use('/product', product);
-app.use('/user', verifyUser, user);
-app.use('/order', verifyUser, order);
-app.use('/payment', verifyUser, stripe);
+app.use('/auth', auth)
+app.use('/product', product)
+app.use('/user', verifyUser, user)
+app.use('/order', verifyUser, order)
+app.use('/payment', verifyUser, stripe)
 
 app.get('/', (req, res) => {
-    res.send('hi');
-});
+  res.send('hi')
+})
 
 app.listen(PORT, () => {
-    console.log('Sever live at port: ', PORT);
-});
+  console.log('Sever live at port: ', PORT)
+})
