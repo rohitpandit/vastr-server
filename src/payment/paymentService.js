aafdb4639604779571e72f82545bbf752d2a9950
Rohit Pandit <rohit0311@outlook.com>
2022-12-06 08:36:04 +0530

    corrects the payment architecture

--- src/payment/paymentService.js
+++ src/payment/paymentService.js
@@ -1,3 +1,15 @@
-const paymentSerice ={}
+const stripe = require('stripe')(
+    'sk_test_51JGFlFSFYhBO3CRq6LXypCRWIKHGIDCPqDUxcXTtzOye3oT7MKKOKYCcmdFItABSMciqkndWzV1lHxmRalal1Ln600D9AyUqpp'
+);
+
+
+const paymentSerice ={
+    getPaymentIntent : (totalPayable) =>{
+        return stripe.paymentIntents.create({
+            amount: totalPayable * 100,
+            currency: 'inr',
+        });
+    }
+}
 
 module.exports = paymentSerice;
